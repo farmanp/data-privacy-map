389aa7fce40896b3952ae2e6f3947498996485bd
Your Name <your_email@example.com>
2024-02-01 20:24:17 -0700

    GET ALL STATES YEAHHH BABY

--- state-data-privacy-laws.js
+++ state-data-privacy-laws.js
@@ -1,4 +1,10 @@
-var stateDataPrivacyLaws = {
+const stateDataPrivacyLaws = {
+  "0": {
+    name: "Alabama",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
   "1": {
     name: "Alaska",
     laws: ["Alaska Consumer Data Privacy Act"],
@@ -10,6 +16,12 @@ var stateDataPrivacyLaws = {
     laws: ["Arizona Data Privacy Law"],
     pendingLegislation: ["Bill 789: Privacy Rights Expansion"],
     recentSettlements: ["Case 012: Unlawful Data Sharing Penalty"]
+  },
+  "3": {
+    name: "Colorado",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
   },
   "4": {
     name: "Florida",
@@ -23,7 +35,270 @@ var stateDataPrivacyLaws = {
     pendingLegislation: ["Bill 678: Consumer Privacy Safeguards"],
     recentSettlements: []
   },
-  // ... rest of the states ...
+  "6": {
+    name: "Indiana",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "7": {
+    name: "Kansas",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "8": {
+    name: "Maine",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "9": {
+    name: "Massachusetts",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "10": {
+    name: "Minnesota",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "11": {
+    name: "New Jersey",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "12": {
+    name: "North Carolina",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "13": {
+    name: "North Dakota",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "14": {
+    name: "Oklahoma",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "15": {
+    name: "Pennsylvania",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "16": {
+    name: "South Dakota",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "17": {
+    name: "Texas",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "18": {
+    name: "Wyoming",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "19": {
+    name: "Connecticut",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "20": {
+    name: "Missouri",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "21": {
+    name: "West Virginia",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "22": {
+    name: "Illinois",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "23": {
+    name: "New Mexico",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "24": {
+    name: "Arkansas",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "25": {
+    name: "California",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "26": {
+    name: "Delaware",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "28": {
+    name: "Hawaii",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "29": {
+    name: "Iowa",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "30": {
+    name: "Kentucky",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "31": {
+    name: "Maryland",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "32": {
+    name: "Michigan",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "33": {
+    name: "Mississippi",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "34": {
+    name: "Montana",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "35": {
+    name: "New Hampshire",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "36": {
+    name: "New York",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "37": {
+    name: "Ohio",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "38": {
+    name: "Oregon",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "39": {
+    name: "Tennessee",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "40": {
+    name: "Utah",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "41": {
+    name: "Virginia",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "42": {
+    name: "Washington",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "43": {
+    name: "Wisconsin",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "47": {
+    name: "Nebraska",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "48": {
+    name: "South Carolina",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "51": {
+    name: "Idaho",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "52": {
+    name: "Nevada",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "53": {
+    name: "Vermont",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "54": {
+    name: "Lousiana",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  },
+  "55": {
+    name: "Rhode Island",
+    laws: [],
+    pendingLegislation: [],
+    recentSettlements: []
+  }
 };
 
 export { stateDataPrivacyLaws };
